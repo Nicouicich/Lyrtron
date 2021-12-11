948ef8d77430450d7bacbc64d288fcc992061bd1
Agustin Higinio <ahiginio1210@gmail.com>
2021-12-11 18:30:07 -0300

    1st upload

---
+++ assets/js/global.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$(".slider-dev").slick({
+  autoplay: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  dots: true,
+  centerMode: true,
+});
+})
+
